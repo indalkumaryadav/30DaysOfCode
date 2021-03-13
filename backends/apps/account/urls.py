8f0125361dd3fdf6b6520a84f4ca658824c27df3
indalkumaryadav <yadavindal269@gmail.com>
2021-03-13 09:37:21 +0530

    first commit

--- backends/apps/account/urls.py
+++ backends/apps/account/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/', include('apps.account')),
+    # path('admin/', admin.site.urls),
+    # path('account/', include('apps.account')),
 ]
