4722e04538ffc7af569bfe9f2c253d6683b58f65
indalkumaryadav <yadavindal269@gmail.com>
2021-03-13 10:20:27 +0530

    first commit

--- backends/apps/account/models.py
+++ backends/apps/account/models.py
@@ -1,2 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import  ugettext_lazy as _
+
+class CustomUserManager():
+    pass
+
+class Customer(AbstractUser):
+    username=None
+    email=models.EmailField(_('Email Address'),unique=True)
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=[]
+    object=CustomUserManager()
