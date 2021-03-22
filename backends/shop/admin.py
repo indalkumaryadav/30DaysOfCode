4400f6c1f55f183ecdf28aec08fb74471a02afd7
indalkumaryadav <yadavindal269@gmail.com>
2021-03-22 07:32:05 +0530

    first commit

--- backends/shop/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Customer,Category,Cart,Product,ProductView,Brand,Order,CartProduct,Review,Slider,TrendingProduct
-# Register your models here.
-admin.site.register(
-    (Customer,Category,Cart,Product,ProductView,Brand,Order,CartProduct,Review,Slider,TrendingProduct)
-    )
