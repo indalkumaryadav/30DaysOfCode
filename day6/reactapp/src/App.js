a32a01b5acdd702421e3323e418dda9410b29bd9
indalkumaryadav <yadavindal269@gmail.com>
2021-04-15 17:55:26 +0530

    first commit

---
+++ day6/reactapp/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./components/navbar/NavBar";
+import "./app.css";
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+};
+
+export default App;
---
+++ day6/reactapp/src/components/navbar/NavBar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./navbar.css";
+
+const NavBar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <div>
+          <ul className="indal">
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Home</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
