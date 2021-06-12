82c2fc61890174aba3b1c7b69f3064c1272ff0e7
indalkumaryadav <yadavindal269@gmail.com>
2021-06-12 17:11:55 +0530

    first commit

---
+++ day11/myapp/src/App.js
@@ -0,0 +1,11 @@
+import "tailwindcss/tailwind.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1 class="bg-red-900">Tailwind css</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ day11/react-tailwinnd/src/App.js
@@ -0,0 +1,12 @@
+import "./assets/output.css";
+
+const App = () => {
+  return (
+    <div>
+      <p class="text-4xl font-bold">Tailwind css</p>
+      <h1 className="bg-red-900">Indal</h1>
+    </div>
+  );
+};
+
+export default App;
